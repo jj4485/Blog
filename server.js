7a8983560a1d8939eda626d0f9687d908f6bbe88
Jonathan <jj4485@princeton.edu>
2023-07-23 22:13:27 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const fileupload = require('express-fileupload');
+
+let initial_path = path.join(__dirname, "public");
+
+const app = express();
